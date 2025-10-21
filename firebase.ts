72b826ca21fd98ceaff83b4994b5560c5567fb06
rezyami <rachid.ezyami@gmail.com>
2025-10-21 16:54:45 +0100

    push first commit

--- firebase.ts
+++ firebase.ts
@@ -3,20 +3,22 @@ import { initializeApp, getApp, getApps } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyAaA8grxg2mGlgsZyE26Q0XhHmdos7nOUA',
-  authDomain: 'netflix-clone-v2-55b33.firebaseapp.com',
-  projectId: 'netflix-clone-v2-55b33',
-  storageBucket: 'netflix-clone-v2-55b33.appspot.com',
-  messagingSenderId: '635770941028',
-  appId: '1:635770941028:web:2c8c63862ccad7ed2af971',
-  measurementId: 'G-ZYN0LJ1WWL',
-}
+  apiKey: "AIzaSyBhIumGwT6DkPU2dzHYVic9knVNII3hEzY",
+  authDomain: "netflixclone-d3451.firebaseapp.com",
+  projectId: "netflixclone-d3451",
+  storageBucket: "netflixclone-d3451.firebasestorage.app",
+  messagingSenderId: "198451577297",
+  appId: "1:198451577297:web:33a71a8e4272bf1fd920f0",
+  measurementId: "G-9MPPGYEPXM"
+};
+
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
 const db = getFirestore()
@@ -24,3 +26,4 @@ const auth = getAuth()
 
 export default app
 export { auth, db }
+
